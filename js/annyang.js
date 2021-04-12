e5246508012a5e8007f28fbd525fa41d6c8dbfcb
Olivia_Anderson <23885831@edgehill.ac.uk>
2021-04-12 12:08:40 +0100

    Full Site Completed with Voice Control and Comments

--- js/annyang.js
+++ js/annyang.js
@@ -1,10 +1,10 @@
-if (annyang) {
+if (annyang) { //defines the commands - first the text expected to hear, then the function to be called
     var commands = {
-            'hello': function() { 
-                alert('Welcome to Computing Champions! You can now speak to navigate around this website. Try it now by saying "About"'); 
+            'hello': function() { //text expected to hear
+                alert('Welcome to Computing Champions! You can now speak to navigate around this website. Try it now by saying "About"'); //fucntion expected - alert to tell user how to use recognition for this site
             },
-            'home': function () {
-                window.location = "../index.html"
+            'home': function () { //text expected to hear
+                window.location = "../index.html" //function - opens the stated page from navigation
             },
             'about': function () {
                 window.location = "../about.html"
@@ -19,8 +19,10 @@ if (annyang) {
                 window.location = "../contact.html"
             },
     }
-}
+};
 
-annyang.addCommands(commands);
+//adds the stated commands to annyang ready to use
+annyang.addCommands(commands); 
 
+//annyang starts to listen (requires microphone to be enabled when site opened!)
 annyang.start();
